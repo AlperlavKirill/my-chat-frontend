fe487f26fbf2de1be771b877da37589940940571
AlperlavKirill <kirill@alperlav.com>
2023-01-14 23:19:23 +0300

    register page init 2.0

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,12 +1,18 @@
-import { InputContainer, InputField } from "../../utils/styles"
+import { InputContainer, InputField, InputLabel, RegisterButton } from "../../utils/styles"
 import styles from "./index.module.scss";
 
 export const RegisterForm = () => {
     return ( 
-        <form>
-            <InputContainer className={styles.form}> 
-                <InputField></InputField>
+        <form className={styles.form}>
+            <InputContainer> 
+                <InputLabel htmlFor="username">Username</InputLabel> 
+                <InputField type="text" id="username"/>
             </InputContainer>
+            <InputContainer> 
+                <InputLabel htmlFor="password">Password</InputLabel> 
+                <InputField type="password" id="password"/>
+            </InputContainer>
+            <RegisterButton>Create an account</RegisterButton>
         </form>
     )
 }
--- src/utils/styles/index.tsx
+++ src/utils/styles/index.tsx
@@ -2,10 +2,31 @@ import styled from "styled-components"
 
 export const InputField = styled.input`
     background-color: inherit;
+    color: #EEEEEE;
+    margin: 4px 0px;
+    outline: none;
+    border: none;
 `;
 
 export const InputContainer = styled.div`
-    background-color: #131313;
+    background-color: #393E46;
+    padding: 12px 16px;
+    border-radius: 15px;
 `;
 
-export const InputLabel = styled.input``;
+export const InputLabel = styled.label`
+    display: block;
+    color: #EEEEEE;
+    margin: 4px 0px;
+    font-size: 24px;
+    font-weight: bolder;
+`;
+
+export const RegisterButton = styled.button`
+    background-color: #4ECCA3;
+    color: #EEEEEE;
+    font-size: 24px;
+    font-weight: bolder;
+    border-radius: 15px;
+    border: none;
+`;
