a1945d2ad3a39183ccb24fcd5384be7348435249
AlperlavKirill <kirill@alperlav.com>
2023-01-22 13:21:48 +0300

    format + apis

--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,46 +1,77 @@
-import { Link } from "react-router-dom";
-import { InputContainer, InputField, InputLabel, Button } from "../../utils/styles"
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  InputContainer,
+  InputField,
+  InputLabel,
+  Button,
+} from "../../utils/styles";
+import { UserRegisterParams } from "../../utils/types";
 import styles from "./index.module.scss";
+import { postUserRegister } from "../../utils/api/api";
 
 export const RegisterForm = () => {
-    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        console.log("register button pressed");
-        
-        event.preventDefault();
-    };
+  const { register, handleSubmit } = useForm<UserRegisterParams>();
 
-    return ( 
-        <form className={styles.form} onSubmit={onSubmit}>
-            <InputContainer> 
-                <InputLabel htmlFor="username">Username</InputLabel> 
-                <InputField type="text" id="username"/>
-            </InputContainer>
-            <InputContainer> 
-                <InputLabel htmlFor="password">Password</InputLabel> 
-                <InputField type="password" id="password"/>
-            </InputContainer>
+  const onSubmit = async (data: UserRegisterParams) => {
+    console.log(data);
 
-            <section className={styles.halfFieldRow}>
-                <InputContainer> 
-                    <InputLabel htmlFor="firstName">First name</InputLabel> 
-                    <InputField type="text" id="firstName"/>
-                </InputContainer>
-                <InputContainer> 
-                    <InputLabel htmlFor="lastName">Last name</InputLabel> 
-                    <InputField type="text" id="lastName"/>
-                </InputContainer>
-            </section>
+    try {
+      postUserRegister(data);
+      navigate('/')
+    } catch (exeption) {
+      console.log(exeption);
+    }
+  };
 
-            <Button className={styles.button}>Create an account</Button>
+  const navigate = useNavigate();
 
-            <div className={styles.footerText}>
-                <span>
-                    Already have an account?
-                </span>
-                <Link to="/login" >
-                    <span>Login</span>
-                </Link>
-            </div>
-        </form>
-    )
-}
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <InputContainer>
+        <InputLabel htmlFor="username">Username</InputLabel>
+        <InputField
+          type="text"
+          id="username"
+          {...register("username", { required: true })}
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputLabel htmlFor="password">Password</InputLabel>
+        <InputField
+          type="password"
+          id="password"
+          {...register("password", { required: true })}
+        />
+      </InputContainer>
+
+      <section className={styles.halfFieldRow}>
+        <InputContainer>
+          <InputLabel htmlFor="firstName">First name</InputLabel>
+          <InputField
+            type="text"
+            id="firstName"
+            {...register("firstname", { required: false })}
+          />
+        </InputContainer>
+        <InputContainer>
+          <InputLabel htmlFor="lastName">Last name</InputLabel>
+          <InputField
+            type="text"
+            id="lastName"
+            {...register("lastname", { required: false })}
+          />
+        </InputContainer>
+      </section>
+
+      <Button className={styles.button}>Create an account</Button>
+
+      <div className={styles.footerText}>
+        <span>Already have an account?</span>
+        <Link to="/login">
+          <span>Login</span>
+        </Link>
+      </div>
+    </form>
+  );
+};
